3554e0bd50437f2efbd34ec9bb1d78006ae4cf72
GabrielBueno200 <56837996+GabrielBueno200@users.noreply.github.com>
2022-05-11 01:31:08 -0300

    Update WashingServers.py

--- WashingServers.py
+++ WashingServers.py
@@ -7,10 +7,6 @@ class WashingServer(BaseComponentServer):
         print("washing substance...")
 
 
-first_washing = WashingServer('localhost', ServersPorts.first_washing)
-second_washing = WashingServer('localhost', ServersPorts.second_washing)
-third_washing = WashingServer('localhost', ServersPorts.third_washing)
-
-first_washing.run()
-second_washing.run()
-third_washing.run()
+WashingServer('localhost', ServersPorts.first_washing).run()
+WashingServer('localhost', ServersPorts.second_washing).run()
+WashingServer('localhost', ServersPorts.third_washing).run()
